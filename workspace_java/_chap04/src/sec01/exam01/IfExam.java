44e88bfaa3cb8bdb92b12c5a5e3117d87deaa02b
강사최민수 <study.todair@gmail.com>
2025-05-29 17:27:55 +0900

    java - if, switch

--- workspace_java/_chap04/src/sec01/exam01/IfExam.java
+++ workspace_java/_chap04/src/sec01/exam01/IfExam.java
@@ -86,10 +86,116 @@ public class IfExam {
 		// Math.random()
 		// 0 <= Math.random() < 1
 		// 주사위 : 1~6
+		// 0~5로 구해놓고 +1을 해서 1~6까지로 만들겠다
+		
 		// 대충 나머지로 구하는 법
+		// Math.random() : 0.123456789
 		double random2 = Math.random() * 10000;
+		// 1234.56789
+		// (int)1234.56789 >>> 1234
+		// 1234 % 6 : 0 ~ 5
+		// 0 ~ 5 +1 >>> 1 ~ 6
 		int dice = ( (int)random2 ) % 6 + 1;
 		System.out.println("주사위 : "+ dice);
+		
+		// 3 + 2 = 5
+		// 3 + 2 +10 = 5 +10
+		// 0*10 < 5*10 < 10*10
+		// 0 <= Math.random() < 1
+		// 0*6 <= Math.random()*6 < 1*6
+		// 0 <= Math.random()*6 < 6	// 0 ~ 5.999999999999
+		int dice0 = (int)(Math.random()*6);	// 0 ~5
+		dice = dice0 + 1; // 1 ~ 6
+		System.out.println("주사위 : "+ dice);
+		
+		// 로또 번호 : 1~45 >>> 0~44 +1
+		int lotto1 = (int)(Math.random()*45) + 1; // 1~45
+		System.out.println("lotto1: "+ lotto1);
+		
+		
+		int num = 1;
+		if( num == 1 ) {
+			System.out.println("num은 1입니다");
+		} else if( num == 2 ) {
+			System.out.println("num은 2입니다");
+		} else {
+			System.out.println("num은 1과 2가 아닙니다");
+		}
+		
+		// 사용가능 : char, byte, short, int, String
+		// 사용못함 : boolean, long, float, double
+		switch(num) {
+			case 1 : 
+				System.out.println("switch num은 1입니다");
+//				break;
+			case 2 :
+				System.out.println("switch num은 2입니다");
+				break;
+			default :
+				System.out.println("switch num은 1과 2가 아닙니다");
+				break;
+		}
+		
+		// 퀴즈
+		// 월에 따라 계절을 출력하세요
+		// 봄:3,4,5  여름:6,7,8  가을:9,10,11  겨울:12,1,2
+		int month = 1;
+		if(month == 1) {
+			System.out.println("겨울");
+		} else if(month == 2) {
+			System.out.println("겨울");
+		}
+		
+		if( 3 <= month && month <= 5 ) {
+			System.out.println("봄");
+		} if( 6 <= month && month <= 8 ) {
+			System.out.println("여름");
+		} if( 9 <= month && month <= 11 ) {
+			System.out.println("가을");
+//		} if( month == 12 || month == 1 || month == 2 ) {
+//		} if( month == 12 || (month >= 1 && month <= 2) ) {
+		} else {
+			System.out.println("겨울");
+		}
+	
+		switch(month) {
+			case 3: 
+			case 4: 
+			case 5: 
+				System.out.println("봄");
+				break;
+
+			case 6: 
+			case 7: 
+			case 8:
+				System.out.println("여름");
+				break;
+ 		}
+	
+		int i = 10;
+		if(i > 5) {
+			System.out.println("5보다 크다");
+		}
+		
+		if(i > 5)
+			System.out.println("5보다 크다");
+
+		if(i < 0) i = 0;
+		
+		if(i > 50)
+			System.out.println("50보다 크다");
+			System.out.println("양수");
+		
+		if(i>0)
+			if(i>5)
+					System.out.println("5보다 크다");
+			else
+				System.out.println("5보다 작다");
+		else
+			System.out.println("음수");
+
+		int studyBoss = ((int) (Math.random()*10) ) + 1;
+		System.out.println("스터디 보스 : "+ studyBoss);
 	}
 
 }
