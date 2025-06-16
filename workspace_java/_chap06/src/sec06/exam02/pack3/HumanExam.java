cc9b26e38d04a965edc856814f1f37179efebb28
강사최민수 <study.todair@gmail.com>
2025-06-16 19:28:18 +0900

    java - 클래스 - static, 접근제한자

--- workspace_java/_chap06/src/sec06/exam02/pack3/HumanExam.java
+++ workspace_java/_chap06/src/sec06/exam02/pack3/HumanExam.java
@@ -14,7 +14,16 @@ public class HumanExam {
 ////		Human h2 = new sec06.exam02.pack1.Human();
 //		sec06.exam02.pack1.Human h2 = new sec06.exam02.pack1.Human();
 
+		// import sec06.exam02.pack1.*;
+		new Human(); 	// pack1.Human
+
+		// 하위 패키지의 클래스라서 못가져옴
+//		new Sub1();		// pack1.sub1.Sub1 
 		
+		// import java.lang.* 자동 선언 되어서 import 생략 가능
+		String a = "1";
+		System.out.println();
+		Math.random();
 	}
 
 }
