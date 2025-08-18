d77ba5576a08234c0bc512df193c5e80c49e7c07
study.todair@gmail.com <study.todair@gmail.com>
2025-08-18 15:31:22 +0900

    Servlet - DB 접속

---
+++ workspace_java/_proj2_request/src/main/webapp/WEB-INF/view/calc/input.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>계산기</h1>
+
+<!-- <form method="get" action="../Input2Servlet"> -->
+<form method="get" action="/_proj2_request/Input2Servlet">
+
+	<input type="text" name="num1">
+	<input type="text" name="num2">
+	<input type="submit" value="SEND">
+
+</form>
+
+</body>
+</html>
---
+++ workspace_java/_proj3_dbcp/src/main/java/todo/DAO/TodoDAO.java
@@ -0,0 +1,80 @@
+package todo.DAO;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import todo.DTO.TodoDTO;
+
+// DAO : Data Access Object 
+public class TodoDAO {
+
+	// tbl_todo의 모든 항목을 돌려주는 메소드
+	// 메소드명 : selectAll
+	// 전달인자 : 없음
+	// 리턴타입 : List<TodoDTO>
+	public List<TodoDTO> selectAll() {
+
+		List<TodoDTO> list = new ArrayList<TodoDTO>();
+		
+		try {
+			// JNDI 방식으로
+			// context.xml에 있는 DB 정보를 가져온다
+			Context ctx = new InitialContext();
+			// DataSource : 커넥션 풀 관리자
+			DataSource dataFactory = (DataSource) ctx.lookup("java:/comp/env/jdbc/oracle");
+
+			// DB 접속
+			Connection conn = dataFactory.getConnection();
+
+			// SQL 준비
+			String query = "select * from tbl_todo";
+			PreparedStatement ps = conn.prepareStatement(query);
+
+			// SQL 실행 및 결과 확보
+			ResultSet rs = ps.executeQuery();
+
+			// 결과 활용
+			while (rs.next()) {
+				// 전달인자로 컬럼명(대소문자 구분없음)
+				int tno = rs.getInt("tno");
+				System.out.println("tno : "+ tno);
+				String title = rs.getString("title");
+				Date duedate = rs.getDate("duedate");
+				int finished = rs.getInt("finished");
+				
+//				Map map = new HashMap();
+//				map.put("tno", tno);
+//				map.put("title", title);
+//				map.put("duedate", duedate);
+//				map.put("finished", finished);
+//				list.add(map);
+				
+				TodoDTO todoDTO = new TodoDTO();
+				todoDTO.setTno(tno);
+				todoDTO.setTitle(title);
+				todoDTO.setDuedate(duedate);
+				todoDTO.setFinished(finished);
+				
+				list.add(todoDTO);
+			}
+
+			rs.close();
+			ps.close();
+			conn.close(); // 커넥션 풀로 반환
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return list;
+	}
+}
